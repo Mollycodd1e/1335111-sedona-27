524faa4b693cf54cd2e11c8596139d2ab1fc41f0
Mollycodd1e <selik7@mail.ru>
2020-02-26 15:21:40 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,10 +1,20 @@
+    // var button = document.querySelector(".button-search-hotel");
+    // var form = document.querySelector(".travel-form");
+
+
+    // button.addEventListener("click",function(evt) {
+    //     evt.preventDefault();
+    //     form.classList.toggle("travel-form-close");
+    // });
+
+
     var button = document.querySelector(".button-search-hotel");
     var form = document.querySelector(".travel-form");
 
 
-    form.classList.add('travel-form-hidden');
+    form.classList.add('travel-form-close');
 
-    button.addEventListener("click",function(evt) {
+    button.addEventListener("click",function() {
         form.classList.toggle("travel-form-open");
         form.classList.toggle("travel-form-close");
     });
