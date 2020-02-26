bf43f2bd0ddb71f823bc1c3150aedba605871494
Mollycodd1e <selik7@mail.ru>
2020-02-26 15:08:05 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,13 +1,3 @@
-    // var button = document.querySelector(".button-search-hotel");
-    // var form = document.querySelector(".travel-form");
-
-
-    // button.addEventListener("click",function(evt) {
-    //     evt.preventDefault();
-    //     form.classList.toggle("travel-form-close");
-    // });
-
-
     var button = document.querySelector(".button-search-hotel");
     var form = document.querySelector(".travel-form");
 
