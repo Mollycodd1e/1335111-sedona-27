cabd37094739dc85f72a834274b59198d2f2b97d
Mollycodd1e <selik7@mail.ru>
2020-02-26 21:50:27 +0300

    js css form

--- js/form.js
+++ js/form.js
@@ -1,12 +1,32 @@
+     // var button = document.querySelector(".button-search-hotel");
+    // var form = document.querySelector(".travel-form");
+
+
+    // button.addEventListener("click",function(evt) {
+    //     evt.preventDefault();
+    //     form.classList.toggle("travel-form-close");
+    // });
+
+
     var button = document.querySelector(".button-search-hotel");
     var form = document.querySelector(".travel-form");
 
-
-    form.classList.add('travel-form-hidden');
+    form.classList.add('travel-form-hide');
+    form.classList.add('travel-form-close');
 
     button.addEventListener("click",function() {
-        form.classList.toggle("travel-form-open");
-        form.classList.toggle("travel-form-close");
+      form.classList.remove('travel-form-hide');
+      // if (form.classList.contains ('travel-form-hide')) {
+      //   form.classList.remove('travel-form-hide');
+      //   form.classList.add('travel-form-open');
+      // } else
+      if (form.classList.contains ('travel-form-close')) {
+        form.classList.remove('travel-form-close');
+        form.classList.add('travel-form-open');
+      }
+      else {
+        form.classList.remove('travel-form-open');
+        form.classList.add('travel-form-close');
+      }
     });
-
 
