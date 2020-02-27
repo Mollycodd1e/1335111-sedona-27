d962b0921c161a3fc56a0946af7e3702cf7153c3
Mollycodd1e <selik7@mail.ru>
2020-02-27 12:53:44 +0300

    js

--- js/form.js
+++ js/form.js
@@ -1,13 +1,11 @@
 var button = document.querySelector(".button-search-hotel");
 var form = document.querySelector(".travel-form");
-var arrival = form.querySelector("[name=arrivaldate]");
 
 form.classList.add('travel-form-hide');
 button.addEventListener("click",function() {
   if (form.classList.contains ('travel-form-hide')) {
     form.classList.remove('travel-form-hide');
     form.classList.add('travel-form-open');
-    arrival.focus();
   }
   else {
     form.classList.remove('travel-form-open');
