524fab40e595deb04d8c5e67de5cb7b37c64a1aa
Mollycodd1e <selik7@mail.ru>
2020-02-27 15:30:48 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -7,24 +7,15 @@ var childrens = form.querySelector("[name=childrens]");
 
 form.classList.add('travel-form-hide');
 button.addEventListener("click",function() {
-  if (form.classList.contains ('travel-form-hide')) {
-    form.classList.remove('travel-form-hide');
-    form.classList.add('travel-form-open');
-  }
-  else {
-    form.classList.remove('"travel-form-error');
-    form.classList.remove('travel-form-open');
-    form.classList.add('travel-form-hide');
-  }
-
-  form.addEventListener("submit", function (evt) {
-    if (!arrival.value || !departure.value || !adults.value || !childrens.value) {
-      evt.preventDefault();
-      form.classList.remove("travel-form-error");
-      form.offsetWidth = form.offsetWidth;
-      form.classList.add("travel-form-error");
-    } 
-  });
+  form.classList.remove("travel-form-error");
+  form.classList.toggle('travel-form-open');
 });
 
-
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adults.value || !childrens.value) {
+    evt.preventDefault();
+    form.classList.remove("travel-form-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("travel-form-error");
+  }
+}); 
