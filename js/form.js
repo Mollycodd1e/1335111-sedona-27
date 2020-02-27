8dd2646939ece14e2bb1e1629e7c79e03f105485
Mollycodd1e <selik7@mail.ru>
2020-02-27 14:39:08 +0300

    css shake

--- js/form.js
+++ js/form.js
@@ -1,5 +1,9 @@
 var button = document.querySelector(".button-search-hotel");
 var form = document.querySelector(".travel-form");
+var arrival = form.querySelector("[name=arrivaldate]");
+var departure = form.querySelector("[name=departuredate]");
+var adults = form.querySelector("[name=adults]");
+var childrens = form.querySelector("[name=childrens]");
 
 form.classList.add('travel-form-hide');
 button.addEventListener("click",function() {
@@ -8,9 +12,19 @@ button.addEventListener("click",function() {
     form.classList.add('travel-form-open');
   }
   else {
+    form.classList.remove('"travel-form-error');
     form.classList.remove('travel-form-open');
     form.classList.add('travel-form-hide');
   }
+
+  form.addEventListener("submit", function (evt) {
+    if (!arrival.value || !departure.value || !adults.value || !childrens.value) {
+      evt.preventDefault();
+      form.classList.remove("travel-form-error");
+      form.offsetWidth = form.offsetWidth;
+      form.classList.add("travel-form-error");
+    } 
+  });
 });
 
 
